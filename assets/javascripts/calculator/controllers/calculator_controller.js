332c5de8000810ad465a847c453f958af8da17c3
pjaramillo <peluja1012@hotmail.com>
2014-10-04 14:49:27 -0400

    Don't allow multiple decimal points of multiple leading 0's

--- assets/javascripts/calculator/controllers/calculator_controller.js
+++ assets/javascripts/calculator/controllers/calculator_controller.js
@@ -21,11 +21,25 @@ var CalculatorController = Ember.ObjectController.extend({
         this.set('result', value);
       } else {
         if(this.get('operationActive')) {
-          value = operand2 + operand;
+          // Don't allow user to input multiple decimal points
+          if(operand2.indexOf('.') !== -1 && operand === '.') return;
+          // Don't allow user to input multiple leading 0's
+          if(operand2 === '0') {
+            value = operand;
+          } else {
+            value = operand2 + operand;
+          }
           this.set('operand2', value);
           this.set('result', value);
         } else {
-          value = operand1 + operand;
+          // Don't allow user to input multiple decimal points
+          if(operand1.indexOf('.') !== -1 && operand === '.') return;
+          // Don't allow user to input multiple leading 0's
+          if(operand1 === '0') {
+            value = operand;
+          } else {
+            value = operand1 + operand;
+          }
           this.set('operand1', value);
           this.set('result', value);
         }
@@ -65,6 +79,7 @@ var CalculatorController = Ember.ObjectController.extend({
         case 'C':
           this.set('operationActive', false);
           this.set('shouldReset', true);
+          this.set('operation', '');
           this.set('operand1', '0');
           this.set('operand2', '');
           this.set('result', '0');
