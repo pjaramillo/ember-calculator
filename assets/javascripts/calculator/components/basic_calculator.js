0ab986fab798c341e800c4aa52386dd5cbc43b41
pjaramillo <peluja1012@hotmail.com>
2014-10-04 10:16:39 -0400

    Implement basic calculator logic

--- assets/javascripts/calculator/components/basic_calculator.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-var BasicCalculator = Ember.Component.extend({
-  classNames: ['basic-calculator']
-});
-
-export default BasicCalculator;
---
+++ assets/javascripts/calculator/components/basic_calculator_component.js
@@ -0,0 +1,108 @@
+import Ember from 'ember';
+
+var BasicCalculator = Ember.Component.extend({
+  classNames: ['basic-calculator'],
+  operation: '',
+  operand1: '0',
+  operand2: '',
+  result: '0',
+  operationActive: false,
+  shouldReset: true,
+
+  actions: {
+    operandClicked: function(operand) {
+      var operand1 = this.get('operand1');
+      var operand2 = this.get('operand2');
+      var value;
+      if(this.get('shouldReset')) {
+        this.set('shouldReset', false);
+        value = operand;
+        this.set('operand1', value);
+        this.set('operand2', '');
+        this.set('result', value);
+      } else {
+        if(this.get('operationActive')) {
+          value = operand2 + operand;
+          this.set('operand2', value);
+          this.set('result', value);
+        } else {
+          value = operand1 + operand;
+          this.set('operand1', value);
+          this.set('result', value);
+        }
+      }
+    },
+
+    operationClicked: function(op) {
+      var value;
+      this.set('shouldReset', false);
+      switch(op) {
+        case '/':
+        case '*':
+        case '-':
+        case '+':
+          this.calculate(this.get('operation'));
+          this.set('operation', op);
+          this.set('operationActive', true);
+          break;
+        case '+/-':
+          this.set('operationActive', false);
+          value = this.get('operand1') * -1;
+          this.set('operand1', value);
+          this.set('result', value);
+          break;
+        case '%':
+          this.set('operationActive', false);
+          value = this.get('operand1') / 100;
+          this.set('operand1', value);
+          this.set('result', value);
+          break;
+        case '=':
+          this.set('operationActive', false);
+          this.set('shouldReset', true);
+          this.calculate(this.get('operation'));
+          this.set('operation', '');
+          break;
+        case 'C':
+          this.set('operationActive', false);
+          this.set('shouldReset', true);
+          this.set('operand1', '0');
+          this.set('operand2', '');
+          this.set('result', '0');
+          break;
+      }
+    }
+  },
+
+  calculate: function(op) {
+    var operand1 = this.get('operand1');
+    var operand2 = this.get('operand2');
+    var value;
+    if(operand2) {
+      switch(op) {
+        case '+':
+          value = (parseFloat(operand1) + parseFloat(operand2)).toString();
+          break;
+        case '-':
+          value = (parseFloat(operand1) - parseFloat(operand2)).toString();
+          break;
+        case '*':
+          value = (parseFloat(operand1) * parseFloat(operand2)).toString();
+          break;
+        case '/':
+          value = (parseFloat(operand1) / parseFloat(operand2)).toString();
+          break;
+      }
+
+    } else {
+      value = parseFloat(operand1).toString();
+    }
+
+    this.set('operand1', value);
+    this.set('operand2', '');
+    this.set('result', value);
+  }
+
+});
+
+export default BasicCalculator;
